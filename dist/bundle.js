897349f30edf89c57b413a8d4cf2a72ce47465ad
anita <epp.anita@icloud.com>
2021-04-18 06:52:01 -0500

    add logo, initial hero styles

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-$(document).ready((function(){console.log("document ready"),$(window).scroll((function(){var o=$(window).scrollTop()/2+"px";$(".parallax").css("transform","translateY("+o+")")}))}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (() => {
+
+eval("// import './someModule'\n$(document).ready(function () {\n  console.log(\"document ready\"); // Parallax\n\n  function simpleParallax(intensity, element) {\n    $(window).scroll(function () {\n      var scrollTop = $(window).scrollTop();\n      var imgPos = scrollTop / intensity + 'px';\n      $(element).css('transform', 'translateY(' + imgPos + ')');\n    });\n  }\n\n  simpleParallax(2, '.parallax');\n});\n\n//# sourceURL=webpack://roper-good-neighbor-fund/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
